927e00522a05e72cc56fe140842b09a66faeb06d
Kyle <kyle.e.cardwell@gmail.com>
2021-08-08 20:14:46 -0600

    added count up & down buttons

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@material-ui/core'
 import { connect } from 'react-redux';
-import { resetTime, startCount, setTimeRemaining, setTimeIsUp } from '../actions'
+import { resetTime, startCount, setTimeRemaining, setTimeIsUp, timerCountDown, timerCountUp } from '../actions'
 
 function Timer(props) {
+
+    const [ counters, showCounters ] = useState(false)
 
     useEffect(() => {
         countDown()
@@ -31,35 +33,53 @@ function Timer(props) {
 
     }
 
+    const setTimeToggle = () => {
+        showCounters(!counters)
+  
+    }
+
     
     return (
         <div className="timer">
 
+            <div className="board-buttons">
 
-            <h3>{props.seconds > 0 ? props.seconds : "TIME IS UP!"}</h3>
+                {counters === true ? <Button className="counterButton"
+                    variant="contained"
+                    onClick={() => props.timerCountDown()}
+                    color="secondary"
+                >Count Down</Button> : ""}
+
+                <h3>{props.seconds > 0 ? props.seconds : "TIME IS UP!"}</h3>
+                
+                {counters === true ? <Button className="counterButton"
+                    variant="contained"
+                    onClick={() => props.timerCountUp()}
+                    color="secondary"
+                >Count Up</Button> : ""}
+            </div>
             
             <div className="board-buttons">
 
-                <Button
+                {counters === false ? <Button
                     variant="contained"
                     onClick={() => props.startCount()}
                     color="secondary"
-                >Start Timer</Button>
+                >Start Timer</Button> : ""}
 
                 <Button
                     variant="contained"
+                    onClick={() => setTimeToggle()}
+                    color="secondary"
+                >{counters === false ? "Set Time" : "OK" }</Button>
+
+                {counters === false? <Button
+                    variant="contained"
                     onClick={() => props.resetTime()}
                     color="secondary"
-                >Reset Timer</Button>
+                >Reset Timer</Button> : ""}
 
             </div>
-
-
-            {/* <Countdown
-                date={Date.now() + 120000}
-            >
-                <Completionist />
-            </Countdown> */}
 
 
         </div>
@@ -69,8 +89,9 @@ function Timer(props) {
 const mapStateToProps = state => {
     return ({
         isCounting: state.isCounting,
-        seconds: state.remaining
+        seconds: state.remaining,
+        setTime: state.setTime
     })
 }
 
-export default connect(mapStateToProps, {resetTime, startCount, setTimeRemaining, setTimeIsUp})(Timer)
+export default connect(mapStateToProps, {resetTime, startCount, setTimeRemaining, setTimeIsUp, timerCountUp, timerCountDown})(Timer)
